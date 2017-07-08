26ca0680c7cd1dbbb00a6648cd546e18b00188aa
Siwawes Wongcharoen <siwaoh@gmail.com>
2017-07-08 22:32:18 +0700

    convert to kotlin

---
+++ app/src/main/java/com/siwaweswongcharoen/my3rdtvapplication/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.siwaweswongcharoen.my3rdtvapplication
+
+import android.app.Application
+
+/**
+ * Created by siwaweswongcharoen on 7/8/2017 AD.
+ */
+
+class MyApplication : Application() 
